e29d8a8609766826a9837b0b6e46b2979e1824ef
QSP <QSP@QSP>
2022-04-05 09:51:27 +0530

    hi

---
+++ src/test/java/scripts/Test5.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test5 {
+	@Test
+	public void tc5() {
+		Reporter.log("Hello postman",true);
+	}
+}
---
+++ src/test/java/scripts/Test6.java
@@ -0,0 +1,11 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+	@Test
+	public void tc5() {
+		Reporter.log("Hello Restassured",true);
+	}
+}

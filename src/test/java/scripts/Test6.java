33e499c7903f472755aff03fac35d8a833e6fe3a
QSP <QSP@QSP>
2022-04-22 07:14:14 +0530

    Hello

---
+++ src/test/java/scripts/Test6.java
@@ -0,0 +1,12 @@
+package scripts;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test6 {
+	@Test
+	public void tc6() {
+		Reporter.log("Hello API",true);
+	}
+
+}
